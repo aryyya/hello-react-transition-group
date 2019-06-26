077db5c968264c2cbf45d0b7cf9563a0f1bbc731
aryyya <aryyya.xyz@gmail.com>
2019-06-25 20:56:07 -0700

    Transition (badly) between two "pages".

--- src/App.js
+++ src/App.js
@@ -2,29 +2,49 @@ import React from 'react'
 import './App.css'
 
 import {
-  BrowserRouter as Router,
-  Route
+  Route,
+  Switch,
+  withRouter
 } from 'react-router-dom'
+
+import {
+  CSSTransition,
+  TransitionGroup
+} from 'react-transition-group'
 
 import Menu from './components/menu'
 import Favs from './components/favs'
 import About from './components/about'
 
-const App = () => (
-  <div className="app">
-    <Router>
-      <Menu />
-      <Route
-        exact
-        path="/"
-        component={Favs}
-      />
-      <Route
-        path="/about"
-        component={About}
-      />
-    </Router>
-  </div>
-)
+const App = ({
+  location
+}) => {
+  console.log(location.key)
 
-export default App
+  return (
+    <div className="app">
+      <Menu />
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          classNames="swipe"
+          timeout={500}
+        >
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={Favs}
+            />
+            <Route
+              path="/about"
+              component={About}
+            />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  )
+}
+
+export default withRouter(App)
